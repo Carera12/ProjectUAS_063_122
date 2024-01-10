ee503f314cbef5e2f6d76899f80c10d9de9a1efa
Carera12 <silvicarera123@gmail.com>
2024-01-10 15:57:29 +0700

    bagian MenuViewModel

--- app/src/main/java/com/example/projectuas/model/MenuViewModel.kt
+++ app/src/main/java/com/example/projectuas/model/MenuViewModel.kt
@@ -1,2 +1,23 @@
 package com.example.projectuas.model
 
+import androidx.lifecycle.ViewModel
+import com.example.projectuas.data.Order
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MenuViewModel : ViewModel() {
+    private val _stateUI = MutableStateFlow(Order())
+    val stateUI: StateFlow<Order> = _stateUI.asStateFlow()
+
+    fun setMakanan(pilih:String){
+        _stateUI.update { currentState -> currentState.copy(makanan = pilih)
+        }
+    }
+
+    fun setMinuman(pilih2:String){
+        _stateUI.update { currentState -> currentState.copy(minuman = pilih2)
+        }
+    }
+}
